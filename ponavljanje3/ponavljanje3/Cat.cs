2d9feb5ac6d9fe2d87a6ff9489f4b84b06d04b17
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-06-13 12:02:35 +0200

    Add files via upload

---
+++ ponavljanje3/ponavljanje3/Cat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponavljanje3
+{
+    public class Cat
+    {
+        bool opasna;
+        public void Opasnost(bool opasna)
+        {
+            opasna = false;
+        }
+
+    }
+
+    public class Gepard : Cat
+    {
+        public void Opasnost(bool opasna)
+        {
+            opasna = true;
+
+            Console.WriteLine("Gepard je opasan." + opasna);
+        }
+        
+
+    }
+}
---
+++ ponavljanje3/ponavljanje3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ponavljanje3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gepard g = new Gepard();
+
+            bool opasna=false;
+
+            g.Opasnost(opasna);
+
+            Console.ReadKey();
+        }
+    }
+}
